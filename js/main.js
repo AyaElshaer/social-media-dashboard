65757157d424652b6f46acb50bfd982341c8df79
aya446 <ayaramadan7799@gmail.com>
2022-02-27 00:29:27 +0200

    final commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
-const theme_btn = document.getElementById("theme-btn");
+// const darkModeBtn = document.getElementById("theme-btn");
+// const switcher = document.querySelector(".switcher");
+// const html = document.querySelector("html");
+// const cards = document.querySelectorAll(".card");
+// const heading = document.querySelector(".heading");
+// const paragraphs = document.querySelectorAll("main p");
+// const socialHeaders = document.querySelectorAll(".social h2");
+// const overviewTitle = document.querySelector(".overview h2");
+
+// darkModeBtn.addEventListener("click", function () {
+
+//   switcher.classList.toggle("active");
+//   darkModeBtn.classList.toggle("toggle");
+//   html.classList.toggle("light");
+//   heading.classList.toggle("heading-light");
+//   overviewTitle.classList.toggle("overview_light");
+
+//   paragraphs.forEach(paragraph => {
+//     paragraph.classList.toggle("light-para");
+//   });
+
+//   socialHeaders.forEach(header => {
+//     header.classList.toggle("heading-light");
+//   });
+
+//   cards.forEach(card => {
+//     card.classList.toggle("card-light");
+   
+//   });
+// });
+
+const darkModeBtn = document.getElementById("theme-btn");
 const switcher = document.querySelector(".switcher");
-const body = document.querySelector("body");
-const cards = document.getElementsByClassName("card");
-const heading = document.querySelector(".heading");
-const paras = document.querySelectorAll("main p");
-const headers_2 = document.querySelectorAll(".social h2");
-const overview_h2 = document.querySelector(".overview h2");
+const html = document.querySelector("html");
 
-theme_btn.addEventListener("click", function () {
+
+darkModeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("light-theme");
+  html.classList.toggle("light");
   switcher.classList.toggle("active");
-  theme_btn.classList.toggle("toggle");
-  body.classList.toggle("light");
-  heading.classList.toggle("heading-light");
-  overview_h2.classList.toggle("overview_light");
-
-  document.querySelector(".social-header p").style.fontWeight = "bold"
-
-  for (let i = 0; i < paras.length; i++) {
-    paras[i].classList.toggle("light-para");
-  }
-
-  for (let i = 0; i < headers_2.length; i++) {
-    headers_2[i].classList.toggle("heading-light");
-  }
-
-  for (let i = 0; i <= cards.length; i++) {
-    cards[i].classList.toggle("card-light");
-  }
-});
+  darkModeBtn.classList.toggle("toggle");
+})
